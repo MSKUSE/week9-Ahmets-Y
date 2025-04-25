2d97b747309b32a7aa39caeda9368e7746157067
Ahmets-Y <ahmetsefayildirim@posta.mu.edu.tr>
2025-04-25 12:06:53 +0300

    Add files via upload

--- src/Shape.java
+++ src/Shape.java
@@ -1,34 +1,25 @@
-public class Shape {
+public abstract class Shape{
 
-    private String color = "white";
+    private String color = "White";
 
-    public Shape(){
 
-    }
-    public Shape(String color) {
+    public Shape (String color) {
         this.color = color;
     }
 
-    public String getColor() {
-        return color;
-    }
 
-    public void setColor(String color) {
-        this.color = color;
-    }
+    public abstract double perimeter();
 
-    public double area(){
-        return 0;
-    }
-    public double perimeter(){
-        return 0;
-    }
-
+    public abstract double area();
 
     @Override
     public String toString() {
         return "Shape{" +
                 "color='" + color + '\'' +
                 '}';
+    }
+
+    public Shape(){
+
     }
 }
